b1883ec0fc588dfed1e6a502a6183e89c7785e00
Leslie Shea <leslieyshea@gmail.com>
2019-04-02 14:14:08 -0700

    added drive method with passing test

--- test/car.test.js
+++ test/car.test.js
@@ -1,28 +1,33 @@
 const { Car } = require('../lib/car');
 
 describe('Car class', () => {
+  let car = null;
+
+  beforeEach(() => {
+    car = new Car('BMW', 'M4', 2019, 'black', 1000);
+  });
+
   it('has make property', () => {
-    const car = new Car('BMW');
     expect(car.make).toEqual('BMW');
   });
 
   it('has model property', () => {
-    const car = new Car('BMW', 'M4');
     expect(car.model).toEqual('M4');
   });
 
   it('has year property', () => {
-    const car = new Car('BMW', 'M4', 2019);
     expect(car.year).toEqual(2019);
   });
 
   it('has color property', () => {
-    const car = new Car('BMW', 'M4', 2019, 'black');
     expect(car.color).toEqual('black');
   });
 
-  it('has miles property', () => {
-    const car = new Car('BMW', 'M4', 2019, 'black', 1000);
-    expect(car.miles).toEqual(1000);
+  it('has drivenMiles property', () => {
+    expect(car.drivenMiles).toEqual(1000);
+  });
+
+  it('has drive method', () => {
+    expect(car.drive(3000)).toEqual(3000);
   });
 });
