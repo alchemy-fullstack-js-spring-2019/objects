a5157e36db4b152e0db5b5b20bb0fa6080984b92
emily baier <emilyrbaier@gmail.com>
2019-04-02 13:42:41 -0700

    updated house test wording to be more clear

--- test/house.test.js
+++ test/house.test.js
@@ -1,23 +1,23 @@
 const { House } = require('../lib/house');
 
 describe('house class tests', ()=> {
-    it('will apply a location property', ()=> {
+    it('has a location property', ()=> {
         const house = new House('foster-powell', 2, 3, 3);
         expect(house.location).toEqual('foster-powell');
     });
-    it('will apply a floors property', ()=> {
+    it('has a floors property', ()=> {
         const house = new House('foster-powell', 2, 3, 3);
         expect(house.floors).toEqual(2);
     });
-    it('will apply a bedrooms property', ()=> {
+    it('has a bedrooms property', ()=> {
         const house = new House('foster-powell', 2, 3, 3);
         expect(house.bedrooms).toEqual(3);
     });
-    it('will apply a bathrooms property', ()=> {
+    it('has a bathrooms property', ()=> {
         const house = new House('foster-powell', 2, 3, 3);
         expect(house.bathrooms).toEqual(3);
     });
-    it('will give a price based on equation', ()=> {
+    it('has a price based on equation', ()=> {
         const house = new House('foster-powell', 2, 3, 3);
         expect(house.housePrice()).toEqual(90000);
     });
