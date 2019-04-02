bcff60af57c40b5bc87e0d47c86e0c810879b26f
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-02 13:48:11 -0700

    refactored House code with forEach

--- test/house.test.js
+++ test/house.test.js
@@ -1,24 +1,23 @@
 const House = require('../lib/house');
 
 describe('House class function', () => {
+    let home = null;
+    beforeEach(() => {
+        home = new House('portland', 2, 4, 3);
+    });
     it('has a location property', () => {
-        const home = new House('portland', 2, 4, 3);
         expect(home.location).toEqual('portland');
     });
     it('has a floors property', () => {
-        const home = new House('portland', 2, 4, 3);
         expect(home.floors).toEqual(2);
     });
     it('has a bedrooms property', () => {
-        const home = new House('portland', 2, 4, 3);
         expect(home.bedrooms).toEqual(4);
     });
     it('has a bathrooms property', () => {
-        const home = new House('portland', 2, 4, 3);
         expect(home.bathrooms).toEqual(3);
     });
     it('has a price calculator', () => {
-        const home = new House('portland', 2, 4, 3);
         expect(home.price()).toEqual(20000);
     });
 });
