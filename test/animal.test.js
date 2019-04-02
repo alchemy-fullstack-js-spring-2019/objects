38f900747cfb7976c7b825f191132cfcfcae3541
emily baier <emilyrbaier@gmail.com>
2019-04-02 11:58:11 -0700

    created and tested constructor and factory functions

---
+++ test/animal.test.js
@@ -0,0 +1,31 @@
+const {
+    AnimalConstructor,
+    AnimalFactory
+} = require('../lib/animal');
+
+describe('animal tests', ()=> {
+    describe('animal constructor test', ()=> {
+        it('will add name and species property', ()=> {
+            const zebra = new AnimalConstructor('stripes', 'zebra');
+            expect(zebra).toEqual({ name: 'stripes', species: 'zebra' });
+        });
+        it('animal will speak', ()=> {
+            const zebra = new AnimalConstructor('stripes', 'zebra');
+            expect(zebra.speak()).toEqual('I am a zebra named stripes!');
+        });
+    });
+    describe('animal factory test', ()=> {
+        it('will add name property', ()=> {
+            const zebra = AnimalFactory('stripes', 'zebra');
+            expect(zebra.name).toEqual('stripes');
+        });
+        it('will add species property', ()=> {
+            const zebra = AnimalFactory('stripes', 'zebra');
+            expect(zebra.species).toEqual('zebra');
+        });
+        it('and will speak', ()=> {
+            const zebra = AnimalFactory('stripes', 'zebra');
+            expect(zebra.speak()).toEqual('I am a zebra named stripes!');
+        });
+    });
+});
