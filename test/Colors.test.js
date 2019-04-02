23374b972cfc037cce7e5a837a819463e72c94ad
Leslie Shea <leslieyshea@gmail.com>
2019-04-02 14:59:43 -0700

    added hasColor method with passing tests

--- test/Colors.test.js
+++ test/Colors.test.js
@@ -7,7 +7,24 @@ describe('Colors class', () => {
     colors = new Colors();
   });
   
-  it('has a favorites property', () => {
+  it('has favorites property', () => {
     expect(colors.favorites).toEqual([]);
+  });
+  
+  it('has addColor method that adds colors to favorites array', () => {
+    expect(colors.addColor('red')).toEqual(['red']);
+    expect(colors.addColor('blue')).toEqual(['red', 'blue']);
+    expect(colors.addColor('yellow')).toEqual(['red', 'blue', 'yellow']);
+  });
+
+  it('has hasColor method that checks for colors in favorites array', () => {
+    colors.addColor('red');
+    expect(colors.hasColor('red')).toBeTruthy();
+
+    colors.addColor('blue');
+    expect(colors.hasColor('blue')).toBeTruthy();
+
+    colors.addColor('yellow');
+    expect(colors.hasColor('yellow')).toBeTruthy();
   });
 });
