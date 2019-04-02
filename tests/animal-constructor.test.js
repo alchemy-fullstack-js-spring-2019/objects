3941c1e7c80052257a933155823760e23dc1fa3e
Ben Parisot <me@benparisot.com>
2019-04-02 11:44:55 -0700

    added name and species tests

--- tests/animal-constructor.test.js
+++ tests/animal-constructor.test.js
@@ -5,4 +5,10 @@ describe('animal constructor', () => {
   it('calls speak method on animal and returns string', () => {
     expect(cat.speak()).toBe('Hello');
   });
+  it('checks the animal has a name', () => {
+    expect(cat.name).toBe('Charles');
+  });
+  it('checks the animal has a species', () => {
+    expect(cat.species).toBe('cat');
+  });
 });
--- tests/animal-factory.test.js
+++ tests/animal-factory.test.js
@@ -5,4 +5,10 @@ describe('animal facotry', () => {
   it('returns a string when speak is called on the object', () => {
     expect(cat.speak()).toBe('Hello');
   });
+  it('checks the animal has a name', () => {
+    expect(cat.name).toBe('Sally');
+  });
+  it('checks the animal has a species', () => {
+    expect(cat.species).toBe('cat');
+  });
 });
