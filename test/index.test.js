f5664e4a2158d22af91983840dac3a08631cfcb2
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-02 11:54:49 -0700

    tests passing

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,64 @@
-const { AnimalConstructor } = require('../lib/index');
+const { AnimalConstructor, AnimalFactory, AnimalClass } = require('../lib/index');
 
 describe('AnimalConstructor', () => {
-  it('Does what?', () => {
-    const expected = 
+  it('Creates a new object with constructor', () => {
+    const expected = {
+      name: 'Jack',
+      species: 'dog'
+    };
 
-    const input = 
-
-    const result = 
+    const result = new AnimalConstructor('Jack', 'dog');
 
     expect(result).toEqual(expected);
+  });
+
+  it('Function speak with constructor', () => {
+    const expected = 'Jack, dog';
+
+    const jack = new AnimalConstructor('Jack', 'dog');
+
+    const result = jack.speak();
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('Animal Factory', () => {
+  it('Has a name property', () => {
+    const result = AnimalFactory('Jack', 'dog');
+
+    expect(result.name).toEqual('Jack');
+  });
+
+  it('Has a species property', () => {
+    const result = AnimalFactory('Jack', 'dog');
+
+    expect(result.species).toEqual('dog');
+  });
+
+  it('Has a speak method', () => {
+    const result = AnimalFactory('Jack', 'dog');
+
+    expect(result.speak()).toEqual('Jack, dog');
+  });
+});
+
+describe('Animal Class', () => {
+  it('Has a name property', () => {
+    const result = new AnimalClass('Jack', 'dog');
+
+    expect(result.name).toEqual('Jack');
+  });
+
+  it('Has a species property', () => {
+    const result = new AnimalClass('Jack', 'dog');
+
+    expect(result.species).toEqual('dog');
+  });
+
+  it('Has a speak method', () => {
+    const result = new AnimalClass('Jack', 'dog');
+
+    expect(result.speak()).toEqual('Jack, dog');
   });
 });
