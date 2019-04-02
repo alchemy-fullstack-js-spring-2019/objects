da132ce4d0aae273cc579f1997eea78d9ef05a15
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-02 14:35:07 -0700

    finished car

--- test/car.test.js
+++ test/car.test.js
@@ -2,23 +2,23 @@ const Car = require('../lib/car');
 
 describe('car class', () => {
     it('has a make property', () => {
-        const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
+        const myCar = new Car('honda', 'fit', 2017, 'gray');
         expect(myCar.make).toEqual('honda');
     });
     it('has a model property', () => {
-        const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
+        const myCar = new Car('honda', 'fit', 2017, 'gray');
         expect(myCar.model).toEqual('fit');
     });
     it('has a year property', () => {
-        const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
+        const myCar = new Car('honda', 'fit', 2017, 'gray');
         expect(myCar.year).toEqual(2017);
     });
     it('has a color property', () => {
-        const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
+        const myCar = new Car('honda', 'fit', 2017, 'gray');
         expect(myCar.color).toEqual('gray');
     });
     it('has a miles property', () => {
-        const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
+        const myCar = new Car('honda', 'fit', 2017, 'gray');
         expect(myCar.miles).toEqual(0);
     });
     it('has a method for drive', () => {
@@ -26,11 +26,17 @@ describe('car class', () => {
         myCar.drive(15);
         expect(myCar.miles).toEqual(15);
     });
-    // it('has an x static method', () => {
-    //     expect(Car.colors()).toEqual([
-    //         'red',
-    //         'blue',
-    //         'green'
-    //     ]);
-    // })
+    it('has a makes static method', () => {
+        expect(Car.makes()).toEqual([
+            'honda',
+            'toyota',
+            'ford',
+            'bmw'
+        ]);
+    });
+    it('throws when a car make is invalid', () => {
+        expect(() => {
+            const myBadCar = new Car('red', 'x', 'funmobile');
+        }).toThrow();
+    });
 });
