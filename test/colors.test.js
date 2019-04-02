07602a21d6d22f71cee34725b8853263a4fc58a3
Payton Lower <paytown@me.com>
2019-04-02 15:03:58 -0700

    fav test passing

--- test/colors.test.js
+++ test/colors.test.js
@@ -3,13 +3,21 @@ const Colors = require('../lib/colors');
 describe('Colors tests', () => {
   let colorDetails = null;
   beforeEach(() => {
-    colorDetails = new Colors([]);
+    colorDetails = new Colors();
   });
 
   it('has an empty favorites property', () => {
     expect(colorDetails.favorites).toEqual([]);
   });
-
-
+  
+  describe('fav method', () => {
+    it('might work', () => {
+      colorDetails.addColor('green');
+      expect(colorDetails.favorites).toEqual(['green']);
+      
+      colorDetails.addColor('yellow');
+      expect(colorDetails.favorites).toEqual(['green', 'yellow']);
+    });
+  });
 
 });
