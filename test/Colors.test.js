2f6e1a31b30671a64f5a6226f5305d49c72908c7
Masmics <cherlocker3113@gmail.com>
2019-04-02 15:19:28 -0700

    colors and colors.test functional

--- test/Colors.test.js
+++ test/Colors.test.js
@@ -1,9 +1,21 @@
-const Colors = require('../lib/Car');
+const Colors = require('../lib/Colors');
 
-var colors = new Colors('black');
+//var colors = new Colors('black');
 
 describe('colors class with favorites param', () => {
-  it('has the expected favorite', () => {
-    expect(colors.favorite).toEqual('black');
+  // it('has the expected favorite', () => {
+  //   expect(colors.favorite).toEqual('black');
+  // });
+  let colors = null;
+  beforeEach(() => {
+    colors = new Colors;
   });
+  it('has a list of colors', () => {
+    expect(colors.favorite).toEqual([]);
+  });
+  // it('can check if favorite hasColor', () => {
+  //   colors.addColor('red');
+  //   expect(colors.hasColor('red')).toBeTruthy();
+  //   expect(colors.hasColor('blue')).toBeFalsy();
+  // });
 });
