a4c6e0bbad48ac2926c33d0159c5036c99d72d2c
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-02 14:23:30 -0700

    addColors method for the Colors class passing

--- test/colors.test.js
+++ test/colors.test.js
@@ -5,4 +5,8 @@ describe('messing with colors class', () => {
     const newColors = new Colors();
     expect(newColors).toEqual({ favorites: [] });
   });
+  it('uses the addColor method to add a color to the empty favorites array, returns that array', () => {
+    const newColors = new Colors();
+    expect(newColors.addColor('blue')).toEqual(['blue']);
+  });
 });
