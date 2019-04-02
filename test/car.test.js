8abe618a128edb8222796b810e6d16c9c498058c
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-02 13:44:51 -0700

    added tests for all house properties/passing tests

--- test/car.test.js
+++ test/car.test.js
@@ -5,4 +5,20 @@ describe('car class', () => {
         const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
         expect(myCar.make).toEqual('honda');
     });
+    it('has a model property', () => {
+        const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
+        expect(myCar.model).toEqual('fit');
+    });
+    it('has a year property', () => {
+        const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
+        expect(myCar.year).toEqual(2017);
+    });
+    it('has a color property', () => {
+        const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
+        expect(myCar.color).toEqual('gray');
+    });
+    it('has a miles property', () => {
+        const myCar = new Car('honda', 'fit', 2017, 'gray', 0);
+        expect(myCar.miles).toEqual(0);
+    });
 });
