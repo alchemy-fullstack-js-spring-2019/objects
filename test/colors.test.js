dab699127f2327d4ca0f081d2bde237f4997059b
emily baier <emilyrbaier@gmail.com>
2019-04-02 14:47:33 -0700

    wrote hasColor methods and tests

--- test/colors.test.js
+++ test/colors.test.js
@@ -11,4 +11,12 @@ describe('color tests', ()=> {
     it('if addColor is called it adds a color to the favorites', ()=> {
         expect(myColors.addColor('yellow')).toEqual(['yellow']);
     });
+    it('if color is in favorites return true', ()=> {
+        myColors.addColor('yellow');
+        expect(myColors.hasColor('yellow')).toBeTruthy();
+    });
+    it('if color is not in favorites return false', ()=> {
+        myColors.addColor('red');
+        expect(myColors.hasColor('yellow')).toBeFalsy();
+    });
 });
