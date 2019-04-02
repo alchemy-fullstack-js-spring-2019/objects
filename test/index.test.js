f781f37481674aa733d22cb8b266f42f9fa34cc5
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-02 11:12:29 -0700

    starting testing

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+const { AnimalConstructor } = require('../lib/index');
+
+describe('AnimalConstructor', () => {
+  it('Does what?', () => {
+    const expected = 
+
+    const input = 
+
+    const result = 
+
+    expect(result).toEqual(expected);
+  });
+});
