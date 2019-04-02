42bce1aaac0da810ec6bbc025d52af64737702c7
UncleJerry <jared.myhrberg@gmail.com>
2019-04-02 13:06:54 -0700

    animals in progress

--- test/constructor.test.js
+++ test/constructor.test.js
@@ -1,12 +1,35 @@
-const AnimalConstructor = require('../lib/constructor');
+const { AnimalConstructor, AnimalFactory } = require('../lib/constructor');
 
 describe('animal constructor', () => {
   it('creates animal steve, tiger', () => {
-    expect(new AnimalConstructor('steve', 'tiger')).toEqual(
-      {
-        name: 'steve',
-        species: 'tiger',
-      }
-    );
+    const steve = new AnimalConstructor('steve', 'tiger');
+    expect(steve.name).toEqual('steve');
+  });
+  it('creates animal steve, tiger', () => {
+    const steve = new AnimalConstructor('steve', 'tiger');
+    expect(steve.species).toEqual('tiger');
+  });
+  it('add say hi prototype to AnimalConstructor', () => {
+    const steve = new AnimalConstructor('steve', 'tiger');
+    expect(steve.sayHi()).toEqual('Hi my name is steve');
   });
 });
+
+describe('animal factory', () => {
+  it('creates animal steve, tiger', () => {
+    const steve = AnimalFactory('steve', 'tiger');
+    expect(steve.name).toEqual('steve');
+  });
+  it('creates animal steve, tiger', () => {
+    const steve = AnimalFactory('steve', 'tiger');
+    expect(steve.species).toEqual('tiger');
+  });
+  it('add say hi prototype to animalFactory', () => {
+    const steve = AnimalFactory('steve', 'tiger');
+    expect(steve.sayHi()).toEqual('Hi my name is steve');
+  });
+});
+
+
+
+
