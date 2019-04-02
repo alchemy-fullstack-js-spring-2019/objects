eb24605e977f70f450f5b2f8ec14673193ba8779
Cara Ponzini <ponzini@gmail.com>
2019-04-02 11:35:31 -0700

    created tests and funtions for animals

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,38 @@
+const { animalConstructor } = require('../lib/index');
+const { animalFactory } = require('../lib/index');
+const { Person } = require('../lib/index');
+
 
 describe('animal constructor', () => {
   test('animal function returns string with speak', () => {
-
+    const name = 'steve';
+    const species = 'elephant';
+    const string = animalConstructor(name, species);
+    expect(string).toEqual('steve');
   });
 });
+
+describe('animal factory', () => {
+  test('returns object that takes two params and string', () => {
+    const name = 'steve';
+    const species = 'turtle';
+    const steve = animalFactory(name, species);
+    const object = steve.speak();
+    expect(object).toEqual('Hi, my name is steve');
+    expect(steve.name).toEqual('steve');
+    expect(steve.species).toEqual('turtle');
+  });
+});
+
+
+
+describe('animal class', () => {
+  test('takes two params returns string with speak', () => {
+    const name = 'bobo';
+    const species = 'spider';
+    const bobo = new Person(name, species);
+    const string = bobo.speak();
+    expect(string).toEqual('Yes, my name is still bobo');
+  });
+})
+;
