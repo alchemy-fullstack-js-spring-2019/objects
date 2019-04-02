b3287faa69d485c35fd3c55e3ceb5d4c3aa0cd0f
emily baier <emilyrbaier@gmail.com>
2019-04-02 13:43:29 -0700

    refactored test to make new class global

--- test/house.test.js
+++ test/house.test.js
@@ -1,24 +1,20 @@
 const { House } = require('../lib/house');
 
 describe('house class tests', ()=> {
+    const house = new House('foster-powell', 2, 3, 3);
     it('has a location property', ()=> {
-        const house = new House('foster-powell', 2, 3, 3);
         expect(house.location).toEqual('foster-powell');
     });
     it('has a floors property', ()=> {
-        const house = new House('foster-powell', 2, 3, 3);
         expect(house.floors).toEqual(2);
     });
     it('has a bedrooms property', ()=> {
-        const house = new House('foster-powell', 2, 3, 3);
         expect(house.bedrooms).toEqual(3);
     });
     it('has a bathrooms property', ()=> {
-        const house = new House('foster-powell', 2, 3, 3);
         expect(house.bathrooms).toEqual(3);
     });
     it('has a price based on equation', ()=> {
-        const house = new House('foster-powell', 2, 3, 3);
         expect(house.housePrice()).toEqual(90000);
     });
 });
