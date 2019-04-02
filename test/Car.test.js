e450080408a12485dd4fc6a4b93294d29cd83118
Tommy Tran <tranttommy95@gmail.com>
2019-04-02 14:38:56 -0700

    added conditional so that it throws if no 'valid' makes are put in

--- test/Car.test.js
+++ test/Car.test.js
@@ -1,6 +1,10 @@
 const Car = require('../lib/Car.js');
 
 describe('Car class', () => {
+  let myCar;
+  beforeEach(() => {
+    myCar = new Car('Nissan', 'Altima', 2009, 'blue');
+  });
 
   it('creates a car object ', () => {
     const expected = {
@@ -13,7 +17,7 @@ describe('Car class', () => {
         this.miles += driven;
       }
     };
-    const result = new Car('Nissan', 'Altima', 2009, 'blue');
+    const result = myCar;
     expect(result.make).toEqual(expected.make);
     expect(result.model).toEqual(expected.model);
     expect(result.year).toEqual(expected.year);
@@ -26,7 +30,7 @@ describe('Car class', () => {
   it('returns an updated amount of miles after drive method', () => {
     const expected = 55000;
     const input = 55000;
-    const result = new Car('Nissan', 'Altima', 2009, 'blue');
+    const result = myCar;
     result.drive(input);
     expect(result.miles).toEqual(expected);
   });
@@ -40,5 +44,11 @@ describe('Car class', () => {
     const result = Car.makes();
   
     expect(result).toEqual(expected);
+  });
+});
+
+describe('Throw test', () => {
+  it('throws on invalid car make', () => {
+    expect(() => (new Car('BMW', 'Altima', 2009, 'blue'))).toThrow();
   });
 });
