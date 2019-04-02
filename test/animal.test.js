93073550910460da0acaea6238c7e14fcd818691
emily baier <emilyrbaier@gmail.com>
2019-04-02 12:58:11 -0700

    added functions and tests for new class

--- test/animal.test.js
+++ test/animal.test.js
@@ -1,6 +1,7 @@
 const {
     AnimalConstructor,
-    AnimalFactory
+    AnimalFactory,
+    AnimalClass
 } = require('../lib/animal');
 
 describe('animal tests', ()=> {
@@ -14,6 +15,7 @@ describe('animal tests', ()=> {
             expect(zebra.speak()).toEqual('I am a zebra named stripes!');
         });
     });
+
     describe('animal factory test', ()=> {
         it('will add name property', ()=> {
             const zebra = AnimalFactory('stripes', 'zebra');
@@ -25,6 +27,21 @@ describe('animal tests', ()=> {
         });
         it('and will speak', ()=> {
             const zebra = AnimalFactory('stripes', 'zebra');
+            expect(zebra.speak()).toEqual('I am a zebra named stripes!');
+        });
+    });
+
+    describe('animal class test', ()=> {
+        it('will add name property', ()=> {
+            const zebra = new AnimalClass('stripes', 'zebra');
+            expect(zebra.name).toEqual('stripes');
+        });
+        it('and the species', ()=> {
+            const zebra = new AnimalClass('stripes', 'zebra');
+            expect(zebra.species).toEqual('zebra');
+        });
+        it('and it will speak too', ()=> {
+            const zebra = new AnimalClass('stripes', 'zebra');
             expect(zebra.speak()).toEqual('I am a zebra named stripes!');
         });
     });
