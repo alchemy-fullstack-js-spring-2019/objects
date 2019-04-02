39c91123f8f259224e3a119f29e65e0b0d5dc9e0
Cheri Portman <cheriportman@hotmail.com>
2019-04-02 13:29:13 -0700

    fixed house test for mutiple properties

--- test/house.test.js
+++ test/house.test.js
@@ -3,6 +3,9 @@ const { HouseClass } = require('../lib/house');
 describe('HouseClass', () => {
   it('has a location', () => {
     const house = new HouseClass('city', 'wood', 4, 2);
-    expect(house.location).toEqual('city', 'wood', 4, 2);
+    expect(house.location).toEqual('city');
+    expect(house.floors).toEqual('wood');
+    expect(house.bedrooms).toEqual(4);
+    expect(house.bathrooms).toEqual(2);
   });
 });
