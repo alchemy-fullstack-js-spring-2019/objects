a1672be943c60b2d76fd3baa15be1259667ba9f1
Leslie Shea <leslieyshea@gmail.com>
2019-04-02 13:48:15 -0700

    refactored tests

--- test/house.test.js
+++ test/house.test.js
@@ -1,23 +1,24 @@
 const { House } = require('../lib/house');
 
 describe('House class', () => {
+  let house = null;
+  beforeEach(() => {
+    house = new House('Portland', 2, 5, 3);
+  });
+
   it('has location property', () => {
-    const house = new House('portland');
-    expect(house.location).toEqual('portland');
+    expect(house.location).toEqual('Portland');
   });
 
   it('has floors property', () => {
-    const house = new House('portland', 3);
-    expect(house.floors).toEqual(3);
+    expect(house.floors).toEqual(2);
   });
 
   it('has bedrooms property', () => {
-    const house = new House('portland', 3, 5);
     expect(house.bedrooms).toEqual(5);
   });
 
   it('has bathrooms property', () => {
-    const house = new House('portland', 3, 5, 4);
-    expect(house.bathrooms).toEqual(4);
+    expect(house.bathrooms).toEqual(3);
   });
 });
