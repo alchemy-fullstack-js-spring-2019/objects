09497c12aef32f4183670f0b9d853c98182c3a0c
mcnadams <mcneilbo@oregonstate.edu>
2019-04-02 11:29:35 -0700

    TDD AnimalClass

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { AnimalConstructor, AnimalFactory } = require('../lib/index');
+const { AnimalConstructor, AnimalFactory, AnimalClass } = require('../lib/index');
 
 describe('constructor function', () => {
     it('makes a dog and the dog speaks', () => {
@@ -13,5 +13,13 @@ describe('factory function', () => {
         const cat = new AnimalFactory('Barry', 'black cat');
         const result = cat.speak();
         expect(result).toBe('My name is Barry and I am a black cat');
+    });
+});
+
+describe('class', () => {
+    it('makes a whale and the whale speaks', () => {
+        const whale = new AnimalClass('George', 'sperm whale');
+        const result = whale.speak();
+        expect(result).toBe('My name is George and I am a sperm whale');
     });
 });
