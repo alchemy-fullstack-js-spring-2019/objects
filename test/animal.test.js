7d075f235bb907ddb3d778a7a683b1e520cd25c6
Tommy Tran <tranttommy95@gmail.com>
2019-04-02 11:40:57 -0700

    AnimalFactory test passed

--- test/animal.test.js
+++ test/animal.test.js
@@ -1,10 +1,31 @@
 const { AnimalConstructor, AnimalFactory } = require('../lib/animal.js');
 
+
 describe('Animal Constructor', () => {
   it('has boots speak', () => {
     const expected = 'I\'m Boots, the monkey!';
     const input = ['Boots', 'monkey'];
     const boots = new AnimalConstructor(...input);
+    const result = boots.speak();
+
+    expect(result).toEqual(expected);
+  });
+
+  it('has swiper speak', () => {
+    const expected = 'I\'m Swiper, the fox!';
+    const input = ['Swiper', 'fox'];
+    const swiper = new AnimalConstructor(...input);
+    const result = swiper.speak();
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('Animal Factory', () => {
+  it('has boots speak', () => {
+    const expected = 'I\'m Boots, the monkey!';
+    const input = ['Boots', 'monkey'];
+    const boots = AnimalFactory(...input);
     const result = boots.speak();
 
     expect(result).toEqual(expected);
