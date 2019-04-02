532abf8ba4c419eb96e2460213f1a5bcee8c5025
heather <unphurlng@gmail.com>
2019-04-02 14:08:21 -0700

    all properties GREEN

--- test/car.test.js
+++ test/car.test.js
@@ -5,5 +5,25 @@ describe('Car Class', () => {
     const car1 = new Car('Honda', 'Odyssey', 2008, 'Silver', 100000);
     expect(car1.make).toEqual('Honda');  
   });
+  
+  it('Has MODEL property', () => {
+    const car1 = new Car('Honda', 'Odyssey', 2008, 'Silver', 100000);
+    expect(car1.model).toEqual('Odyssey');  
+  });
+
+  it('Has YEAR property', () => {
+    const car1 = new Car('Honda', 'Odyssey', 2008, 'Silver', 100000);
+    expect(car1.year).toEqual(2008);  
+  });
+
+  it('Has COLOR property', () => {
+    const car1 = new Car('Honda', 'Odyssey', 2008, 'Silver', 100000);
+    expect(car1.color).toEqual('Silver');  
+  });
+
+  it('Has MILES property', () => {
+    const car1 = new Car('Honda', 'Odyssey', 2008, 'Silver', 100000);
+    expect(car1.miles).toEqual(100000);  
+  });
 
 });
