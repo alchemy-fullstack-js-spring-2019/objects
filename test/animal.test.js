7f753286cb3a0e8b48abde910381bb598c5d1b05
Leslie Shea <leslieyshea@gmail.com>
2019-04-02 11:52:45 -0700

    wrote/passed AnimalConstrutor and AnimalFactory functions

---
+++ test/animal.test.js
@@ -0,0 +1,54 @@
+const { AnimalConstructor, AnimalFactory } = require('../lib/animal');
+
+describe('animal object creators', () => {
+  describe('AnimalConstructor function', () => {
+    it('has a name property', () => {
+      const dog = new AnimalConstructor('spot');
+      expect(dog.name).toEqual('spot');
+    });
+
+    it('has a species property', () => {
+      const dog = new AnimalConstructor('spot', 'dog');
+      expect(dog.species).toEqual('dog');
+    });
+
+    it('can speak its name and species', () => {
+      const dog = new AnimalConstructor('spot', 'dog');
+      expect(dog.speak()).toEqual('Hi, my name is spot, and I am a dog.');
+    });
+  });
+
+  describe('AnimalFactory function', () => {
+    it('has a name property', () => {
+      const dog = new AnimalFactory('spot');
+      expect(dog.name).toEqual('spot');
+    });
+
+    it('has a species property', () => {
+      const dog = new AnimalConstructor('spot', 'dog');
+      expect(dog.species).toEqual('dog');
+    });
+
+    it('can speak its name and species', () => {
+      const dog = new AnimalConstructor('spot', 'dog');
+      expect(dog.speak()).toEqual('Hi, my name is spot, and I am a dog.');
+    });
+  });
+
+  describe('AnimalClass class', () => {
+    // it('has a name property', () => {
+    //   const dog = new AnimalFactory('spot');
+    //   expect(dog.name).toEqual('spot');
+    // });
+
+    // it('has a species property', () => {
+    //   const dog = new AnimalConstructor('spot', 'dog');
+    //   expect(dog.species).toEqual('dog');
+    // });
+
+    // it('can speak its name and species', () => {
+    //   const dog = new AnimalConstructor('spot', 'dog');
+    //   expect(dog.speak()).toEqual('Hi, my name is spot, and I am a dog.');
+    // });
+  });
+});
