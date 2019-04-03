14835afc379ddc0da91b5697e7d0c316bf7107fc
Tommy Tran <tranttommy95@gmail.com>
2019-04-02 20:41:21 -0700

    toString method test passed

--- test/User.test.js
+++ test/User.test.js
@@ -1,18 +1,29 @@
 const User = require('../lib/User.js');
 
 describe('User Class', () => {
+  let user;
+  beforeEach(() => {
+    user = new User('Tommy', 'tommy@tran.com', '******');
+  });
+
   it('creates User object', () => {
     const expected = {
       name: 'Tommy',
       email: 'tommy@tran.com',
       password: '******'
     };
-    const input = ['Tommy', 'tommy@tran.com', '******'];
-    const result = new User(...input);
+    const result = user;
 
     expect(result.name).toEqual(expected.name);
     expect(result.email).toEqual(expected.email);
     expect(result.password).toEqual(expected.password);
     expect.any(result._id);
+  });
+
+  it('returns string of name and email', () => {
+    const expected = 'Tommy | tommy@tran.com';
+    const result = user.toString();
+    
+    expect(result).toEqual(expected);
   });
 });
