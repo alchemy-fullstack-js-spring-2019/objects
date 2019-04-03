88368e3fb99380340ebc8f78902c7e363df82abe
Cheri Portman <cheriportman@hotmail.com>
2019-04-03 08:11:58 -0700

    AnimalConstructor and  prototype speak passes tests

--- test/animal.test.js
+++ test/animal.test.js
@@ -2,9 +2,20 @@ const { AnimalConstructor } = require('../lib/animal');
 
 describe('animal object creators', () => {
   describe('AnimalConstructor', () => {
+
     it('has a name property', () => {
-      const dog = new AnimalConstructor('rover', 'dog');
+      const dog = new AnimalConstructor('rover');
       expect(dog.name).toEqual('rover');
+    });
+
+    it('has a species property', () => {
+      const dog = new AnimalConstructor('rover', 'dog');
+      expect(dog.species).toEqual('dog');
+    });
+
+    it('has a speak method', () => {
+      const dog = new AnimalConstructor('rover', 'dog');
+      expect(dog.speak()).toEqual('My name is rover and I think of myself as a dog');
     });
   });
 });
