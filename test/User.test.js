ec6065b9ac15d6f946662d7336233e9974590479
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-03 13:25:06 -0700

    made user function pass

--- test/User.test.js
+++ test/User.test.js
@@ -13,4 +13,16 @@ describe('user function', () => {
         const profile = new User('blah@blah.com');
         expect(profile.name).toEqual('blah@blah.com');
     });
+    it('has uuid', () => {
+        const profile = new User();
+        expect(profile.id).toEqual(expect.any(String));
+    });
+    it('toString returns name and email', () => {
+        const profile = new User('Fran', 'Fran@fran.com', 'fran');
+        expect(profile.toString()).toEqual('Fran | Fran@fran.com');
+    });
+    it('resetPassword function sets new passwords', () => {
+        const profile = new User('Fran', 'Fran@fran.com', 'fran');
+        expect(profile.resetPassword('fran', 'billy')).toEqual('billy');
+    });
 });
