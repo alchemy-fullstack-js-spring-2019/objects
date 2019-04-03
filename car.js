814a1d0291ea89038198ce53daf5963b95e278b4
Laura Badgley <l.badgleyy@gmail.com>
2019-04-02 23:20:37 -0700

    car obj test passing

--- car.js
+++ car.js
@@ -1,15 +1,14 @@
-class Car {
-  constructor(make, model, year, color, miles) {
+module.exports = class Car {
+  constructor(make, model, year, color) {
     this.make = make;
     this.model = model;
     this.year = year;
     this.color = color;
     this.miles = 0;
   }
-}
-
-// drive(miles) {
-//   this.miles += miles;
-// }
-
-module.exports = Car;
+  
+  drive(miles) {
+    this.miles += miles;
+  }
+  
+};
--- car.test.js
+++ car.test.js
@@ -1,29 +1,40 @@
 const Car = require('./car');
 
 describe('car obj creator', () => {
+  let car = null;
+  beforeEach(() => {
+    car = new Car('Toyota', 'iM', 2017, 'chartreuse');
+  });
+
   it('has a make', () => {
-    const car = new Car ('Toyota');
+    // const car = new Car ('Toyota');
     expect(car.make).toEqual('Toyota');
   });
   it('has a model', () => {
-    const car = new Car ('Toyota', 'iM');
+    // const car = new Car ('Toyota', 'iM');
     expect(car.model).toEqual('iM');
   });
   it('has a year', () => {
-    const car = new Car ('Toyota', 'iM', 2017);
+    // const car = new Car ('Toyota', 'iM', 2017);
     expect(car.year).toEqual(2017);
   });
   it('has a color', () => {
-    const car = new Car ('Toyota', 'iM', 2017, 'chartreuse');
+    // const car = new Car ('Toyota', 'iM', 2017, 'chartreuse');
     expect(car.color).toEqual('chartreuse');
   });
   it('has miles', () => {
-    const car = new Car ('Toyota', 'iM', 2017, 'chartreuse', 0);
+    // const car = new Car ('Toyota', 'iM', 2017, 'chartreuse', 0);
     expect(car.miles).toEqual(0);
   });
-  // it('can be driven', () => {
-  //   expect(car.miles)
-  // })
+  it('can be driven', () => {
+    car.drive(10);
+    expect(car.miles).toEqual(10);
+
+    car.drive(50);
+
+    expect(car.miles).toEqual(60);
+
+  });
 
 
 });
