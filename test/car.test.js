fe5897e240877866d2d9a6dc6a6e9959da2cf020
Cheri Portman <cheriportman@hotmail.com>
2019-04-04 20:08:10 -0700

    refactored car.test  - repeated code

--- test/car.test.js
+++ test/car.test.js
@@ -2,36 +2,41 @@ const Car = require('../lib/car');
 
 
 describe('car constructor', () => {
+
+  let car = null;
+  beforeEach(() => {
+    car = new Car('Honda', 'CRV', 2012, 'red');
+  });
+
   it('has a make', () => {
-    const car = new Car('Honda');
     expect(car.make).toEqual('Honda');
   });
+
   it('has a model', () => {
-    const car = new Car('Honda', 'CRV');
     expect(car.model).toEqual('CRV');
   });
+
   it('has a model', () => {
-    const car = new Car('Honda', 'CRV', 2012);
     expect(car.year).toEqual(2012);
   });
+
   it('has a model', () => {
-    const car = new Car('Honda', 'CRV', 2012, 'red');
     expect(car.color).toEqual('red');
   }); 
+
   it('has miles', () => {
-    const car = new Car('Honda', 'CRV', 2012, 'red');
     expect(car.miles).toEqual(0);
   });
+
   it('can be driven', () => {
-    const car = new Car('Honda', 'CRV', 2012, 'red');
     car.drive(10);
     expect(car.miles).toEqual(10);
     
     car.drive(50);
     expect(car.miles).toEqual(60);
   });
+
   it('has a makes-static method', () => {
-    const car = new Car('Honda', 'CRV', 2012, 'red');
     expect(Car.makes()).toEqual([
       'Honda',
       'toyota',
@@ -41,6 +46,7 @@ describe('car constructor', () => {
   });
   it('throws error when car is made w/invalid make', () => {
     expect(() => {
+      /*eslint-disable-next-line*/
       const badCar = new Car('datsun', 'CRV', 2012, 'red');
     }).toThrow();
   });
